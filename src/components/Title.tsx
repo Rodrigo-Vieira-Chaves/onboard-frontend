26a4d58f338926e3d9d05ce5e00d94c5c942b226
Rodrigo-Vieira-Chaves <rodrigovieirachaves@gmail.com>
2023-02-14 14:15:18 -0400

    Added styles

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,20 @@
+import styled from 'styled-components';
+
 interface TitleProps {
   titleText: string;
+  fontSize?: string;
+  fontWeight?: string;
+  color?: string;
+  margins?: string;
 }
 
+const StyledTitle = styled.h1.attrs((props: TitleProps) => props)`
+  font-size: ${(props) => props.fontSize ?? '24px'};
+  font-weight: ${(props) => props.fontWeight ?? 'bold'};
+  color: ${(props) => props.color ?? 'black'};
+  margin: ${(props) => props.margins ?? '20px top, 20px bottom'};
+`;
+
 export function Title(props: TitleProps) {
-  return <h1>{props.titleText}</h1>;
+  return <StyledTitle>{props.titleText}</StyledTitle>;
 }
